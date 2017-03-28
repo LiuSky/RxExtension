1fe5d04bc14924b39f42d0bd3b99c6f172994d46
xiaobin liu <327847390@qq.com>
2017-03-28 13:13:31 +0800

    add

---
+++ RxExtension/Classes/UICollectionReusableView+Rx.swift
@@ -0,0 +1,47 @@
+//
+//  UICollectionReusableView+Rx.swift
+//  RxExtension
+//
+//  Created by xiaobin liu on 2017/3/28.
+//  Copyright © 2017年 Sky. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+
+private var prepareForReuseBag: Int8 = 0
+
+public extension UICollectionReusableView {
+    
+    public var rx_prepareForReuse: Observable<Void> {
+        return Observable.of(self.rx.sentMessage(#selector(UICollectionReusableView.prepareForReuse)).map { _ in () }, self.rx.deallocated).merge()
+    }
+    
+    
+    public var rx_prepareForReuseBag: DisposeBag {
+        MainScheduler.ensureExecutingOnScheduler()
+        
+        if let bag = objc_getAssociatedObject(self, &prepareForReuseBag) as? DisposeBag {
+            return bag
+        }
+        
+        let bag = DisposeBag()
+        objc_setAssociatedObject(self, &prepareForReuseBag, bag, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+        
+        _ = self.rx.sentMessage(#selector(UICollectionReusableView.prepareForReuse)).subscribe(onNext: { [weak self] _ in
+            let newBag = DisposeBag()
+            objc_setAssociatedObject(self, &prepareForReuseBag, newBag, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+        })
+        
+        return bag
+    }
+}
+
+
+public extension Reactive where Base: UICollectionReusableView {
+    
+    public var prepareForReuseBag: DisposeBag {
+        return base.rx_prepareForReuseBag
+    }
+}
